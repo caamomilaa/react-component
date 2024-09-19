0dd253beeff8bf8b0baecc7d8f068606fa7e9f62
caamomilaa <c.velazquezdamico@gmail.com>
2024-09-19 21:03:32 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 // todos los componentes se exportan
+// Los componentes se ejecutan con la sintaxis de etiqueta.
+
+const Image = props => {
+	return (
+		<div>
+			<h2>{props.object}</h2>
+			<img
+				src={`/assets/images/${props.image}`}
+				alt={`Elemento ${props.object}`}
+				width='150'
+			/>
+		</div>
+	);
+};
 
 const App = () => {
-	return <div></div>;
+	return (
+		<>
+			<h2>Los componentes del diablo</h2>
+			<Image object='Cocktail' image='cocktail.svg' />
+			<Image object='Coffee' image='coffee.svg' />
+			<Image object='Plane' image='plane.svg' />
+			<Image object='Train' image='train.svg' />
+		</>
+	);
 };
 
 export default App;
